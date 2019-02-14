5906a8923beb7b8f18aaf8d2122d9c0e360da96c
Sai Charan <s.charancherry22@gmail.com>
2019-02-14 11:32:11 +0530

    signpage #2

--- test/specs/CheckoutTest.js
+++ test/specs/CheckoutTest.js
@@ -11,30 +11,30 @@ suite('Adding Items to Cart', () => {
     suiteSetup(() => {
         Page.open('https://www.homedepot.com/');
     })
+
     test('searching hammer in the site', () => {
         let elem = Header.searchInputField
         elem.addValue('hammer');
         assert.strictEqual(elem.getValue(), 'hammer');
         Header.searchButton.click();
     })
+
     test('adding first product available in PLP to cart and checking the cart', () => {
         ProductListPage.addToCartButton(ProductListPage.getFirstProductAvailable(ProductListPage.products)).click();
         if(!PageUtills.isCartPage) {
             ThdOverlayWrapper.closeAddToCartOverlay();
-            expect(Header.itemCountInCart()).to.equal(1);
-        } else {
-            expect(CartPage.itemCount).to.equal(1);
         }
+        expect(Header.itemCountInCart()).to.equal(1);
     })
+
     test('proceed to checkout page and guest sign in', () => {
         if(!PageUtills.isCartPage) {
             Header.cartButton.click();
-            expect(CartPage.itemCount).to.equal(1);
-        }
-        if(!CartPage.checkoutButton.isExisting()) {
-            ThdOverlayWrapper.closeConfirmPickupStoreOverlay();
         }
         CartPage.checkoutButton.click();
+        if(!PageUtills.isSigninPage) {
+            ThdOverlayWrapper.closeConfirmPickupStoreOverlay();
+        }
         SigninPage.guestLoginEmailInputField.addValue('midoriya@izuku.com');
         expect(SigninPage.guestLoginEmailInputField.getValue()).to.equal('midoriya@izuku.com');
         SigninPage.guestLoginContinueButton.click();
